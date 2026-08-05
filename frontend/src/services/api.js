9b92ed236b9789e4188ce357f2a88cf897f23dab
VirajShinde7425 <vs922265@gmail.com>
2026-08-06 02:16:01 +0530

    Updated API.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,29 +1,17 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL
-// });
-
-// api.interceptors.request.use((config) => {
-//     const token = localStorage.getItem("token");
-
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-
-//     return config;
-// });
-
-// export default api;
-
 import axios from "axios";
-
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
 
-console.log("Axios Base URL =", api.defaults.baseURL);
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
 
 export default api;
